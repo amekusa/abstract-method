928d4f342911295c7ce7aab15af4b3dca50ad777
amekusa <hello@amekusa.com>
2021-10-08 18:02:18 +0900

    Update test

--- test.js
+++ test.js
@@ -12,6 +12,12 @@ describe(`Abstract`, () => {
 			walk() {
 				return true;
 			}
+			get legs() {
+				return 4;
+			}
+			set sex(x) {
+				this._sex = x;
+			}
 		};
 	});
 
@@ -20,9 +26,11 @@ describe(`Abstract`, () => {
 		assert.equal(c.Animal.walk, undefined);
 		assert.equal(typeof c.Animal.prototype.walk, 'function');
 
+		let r;
+
 		let animal = new c.Animal();
 		assert.throws(() => {
-			animal.walk();
+			r = animal.walk();
 		}, {
 			name: 'NoImpl',
 			info: {
@@ -34,7 +42,7 @@ describe(`Abstract`, () => {
 
 		let snake = new c.Snake();
 		assert.throws(() => {
-			snake.walk();
+			r = snake.walk();
 		}, {
 			name: 'NoImpl',
 			info: {
@@ -45,10 +53,82 @@ describe(`Abstract`, () => {
 		});
 
 		let dog = new c.Dog();
-		let r;
 		assert.doesNotThrow(() => {
 			r = dog.walk();
 		});
 		assert.ok(r);
 	});
+
+	it(`getter`, () => {
+		abstract(c.Animal).getter('legs')
+		assert.equal(c.Animal.legs, undefined);
+
+		let legs = 0;
+
+		let animal = new c.Animal();
+		assert.throws(() => {
+			legs = animal.legs;
+		}, {
+			name: 'NoImpl',
+			info: {
+				class: 'Animal',
+				method: 'legs',
+				declared_in: 'Animal'
+			}
+		});
+
+		let snake = new c.Snake();
+		assert.throws(() => {
+			legs = snake.legs;
+		}, {
+			name: 'NoImpl',
+			info: {
+				class: 'Snake',
+				method: 'legs',
+				declared_in: 'Animal'
+			}
+		});
+
+		let dog = new c.Dog();
+		assert.doesNotThrow(() => {
+			legs = dog.legs;
+		});
+		assert.equal(legs, 4);
+	});
+
+	it(`setter`, () => {
+		abstract(c.Animal).setter('sex');
+		assert.equal(c.Animal.sex, undefined);
+
+		let animal = new c.Animal();
+		assert.throws(() => {
+			animal.sex = 'male';
+		}, {
+			name: 'NoImpl',
+			info: {
+				class: 'Animal',
+				method: 'sex',
+				declared_in: 'Animal'
+			}
+		});
+
+		let snake = new c.Snake();
+		assert.throws(() => {
+			snake.sex = 'mail';
+		}, {
+			name: 'NoImpl',
+			info: {
+				class: 'Snake',
+				method: 'sex',
+				declared_in: 'Animal'
+			}
+		});
+
+		let dog = new c.Dog();
+		assert.doesNotThrow(() => {
+			dog.sex = 'male';
+		});
+		assert.equal(dog._sex, 'male');
+	});
+
 });
